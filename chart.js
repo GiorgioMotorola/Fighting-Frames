129b960feb5709fa1e7bc10f2de2f5a56267395d
Mike Weatherford <97712526+GiorgioMotorola@users.noreply.github.com>
2023-10-23 19:27:59 -0400

    added the js logic including chart data and modal

---
+++ chart.js
@@ -0,0 +1,33 @@
+function createChart(movie1, movie2) {
+  const boxOffice1 = parseInt(movie1.BoxOffice.replace(/\D/g, ""), 10);
+  const boxOffice2 = parseInt(movie2.BoxOffice.replace(/\D/g, ""), 10);
+
+  const xValues = [movie1.Title, movie2.Title];
+  const yValues = [boxOffice1, boxOffice2];
+  const barColors = ["rgba(10, 135, 74, 0.5", "rgba(10, 131, 135, 0.5)"];
+
+  new Chart("boxOfficeChart", {
+    type: "bar",
+    data: {
+      labels: xValues,
+      datasets: [
+        {
+          backgroundColor: barColors,
+          data: yValues,
+          barThickness: 100,
+        },
+      ],
+    },
+    options: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: "Box Office",
+      },
+    },
+  });
+}
+
+export { createChart };
---
+++ main.js
@@ -0,0 +1,187 @@
+import { imdbID } from "./imdbID.js";
+import { apiKey } from "./apiKey.js";
+import { createChart } from "./chart.js";
+
+let score = 0;
+let movieBatch = [];
+let highScore = 0;
+
+function getRandomImdbID() {
+  const randomIndex = Math.floor(Math.random() * imdbID.length);
+  const randomImdbID = imdbID[randomIndex];
+  imdbID.splice(randomIndex, 1);
+  return randomImdbID;
+}
+
+async function getRandomMovie() {
+  const randomImdbID = getRandomImdbID();
+  const url = `${apiKey}&i=${randomImdbID}`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("lol", error);
+  }
+}
+
+function getMovieBatch() {
+  movieBatch = [];
+  const promises = [];
+
+  for (let i = 0; i < 2; i++) {
+    const promise = getRandomMovie().then((movie) => {
+      movieBatch.push(movie);
+    });
+
+    promises.push(promise);
+  }
+
+  Promise.all(promises).then(() => {
+    compareMovies(movieBatch[0], movieBatch[1]);
+  });
+}
+
+function compareMovies(movie1, movie2) {
+  const movieList = document.querySelector(".movieList");
+  movieList.innerHTML = "";
+
+  const movieInfo1 = createMovieInfo(movie1);
+  const movieInfo2 = createMovieInfo(movie2);
+
+  movieInfo1.addEventListener("click", () =>
+    handleGuess(1, movie1.imdbRating, movie2.imdbRating)
+  );
+  movieInfo2.addEventListener("click", () =>
+    handleGuess(2, movie1.imdbRating, movie2.imdbRating)
+  );
+
+  movieList.appendChild(movieInfo1);
+  movieList.appendChild(movieInfo2);
+
+  setTimeout(function () {
+    createChart(movie1, movie2);
+  }, 1350);
+}
+
+function createMovieInfo(movie) {
+  const movieInfo = document.createElement("div");
+  movieInfo.classList.add("movie-info");
+
+  const moviePoster = document.createElement("img");
+  moviePoster.src = movie.Poster;
+  moviePoster.classList.add("movie-poster");
+  moviePoster.addEventListener("click", () => {
+    openModal();
+  });
+
+  const movieTitle = document.createElement("div");
+  movieTitle.classList.add("movie-title");
+  movieTitle.textContent = movie.Title;
+  movieInfo.appendChild(movieTitle);
+  movieInfo.appendChild(moviePoster);
+
+  return movieInfo;
+}
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+  setTimeout(() => {
+    loader.classList.add("loader-hidden");
+    loader.addEventListener("transitionend", () => {
+      loader.style.display = "none";
+    });
+  }, 1400);
+});
+
+function saveScoreToLocalStorage() {
+  localStorage.setItem("score", score.toString());
+
+  if (score > highScore) {
+    highScore = score;
+    localStorage.setItem("highScore", highScore.toString());
+  }
+}
+
+function getHighScoreFromLocalStorage() {
+  const storedHighScore = localStorage.getItem("highScore");
+  if (storedHighScore) {
+    highScore = parseInt(storedHighScore, 10);
+  }
+}
+
+function updateHighScore() {
+  const highScoreElement = document.getElementById("highScore");
+  highScoreElement.textContent = highScore;
+}
+
+getHighScoreFromLocalStorage();
+updateHighScore();
+
+function getScoreFromLocalStorage() {
+  const storedScore = localStorage.getItem("score");
+  if (storedScore) {
+    score = parseInt(storedScore, 10);
+  }
+}
+
+function handleGuess(userGuess, rating1, rating2) {
+  const resultMessage = document.createElement("div");
+  resultMessage.classList.add("result-message");
+
+  const correctAnswer = rating1 > rating2 ? 1 : 2;
+  const isCorrect = userGuess === correctAnswer;
+
+  if (isCorrect) {
+    score += 1;
+  } else {
+    score = 0;
+  }
+
+  saveScoreToLocalStorage();
+}
+
+function openModal() {
+  var modal = document.getElementById("myModal");
+  var imdbRating1Element = document.getElementById("imdbRating1");
+  var imdbRating2Element = document.getElementById("imdbRating2");
+  var imdbPoster1Element = document.getElementById("imdbPoster1");
+  var imdbPoster2Element = document.getElementById("imdbPoster2");
+
+  const imdbRating1 = movieBatch[0].imdbRating;
+  const imdbRating2 = movieBatch[1].imdbRating;
+  imdbPoster1Element.src = movieBatch[0].Poster;
+  imdbPoster2Element.src = movieBatch[1].Poster;
+
+  imdbRating1Element.textContent = imdbRating1;
+  imdbRating2Element.textContent = imdbRating2;
+  imdbPoster1Element.textContent = imdbPoster1;
+  imdbPoster2Element.textContent = imdbPoster2;
+
+  modal.style.display = "block";
+
+  setTimeout(function () {
+    closeAndReloadModal();
+  }, 50000);
+
+  var closeModalBtn = document.getElementById("closeModal");
+  closeModalBtn.addEventListener("click", () => {
+    closeAndReloadModal();
+  });
+}
+
+function closeAndReloadModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+  location.reload();
+}
+
+getScoreFromLocalStorage();
+
+const scoreElement = document.getElementById("score");
+scoreElement.textContent = score;
+
+window.addEventListener("load", getMovieBatch);
+
+window.localStorage.removeItem("score");
